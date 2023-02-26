0901a3a79eb66da629aa927a38f5fadea7b2745b
vangoj <jamesvango24@gmail.com>
2023-02-26 17:55:11 +0000

    Sprint 1
    Sprint 1 - Azure create and destroy added and a textbox to display powershell output within the app

--- FYP_CLOUD_APP/Form1.Designer.cs
+++ FYP_CLOUD_APP/Form1.Designer.cs
@@ -32,24 +32,39 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(185, 148);
+            this.label1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 143);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(387, 15);
+            this.label1.Size = new System.Drawing.Size(550, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Create and destroy an AWS apache web server with the below buttons";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LawnGreen;
-            this.button1.Location = new System.Drawing.Point(188, 180);
+            this.button1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 173);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 54);
+            this.button1.Size = new System.Drawing.Size(167, 66);
             this.button1.TabIndex = 1;
             this.button1.Text = "Create Server";
             this.button1.UseVisualStyleBackColor = false;
@@ -58,27 +73,134 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Crimson;
-            this.button2.Location = new System.Drawing.Point(447, 180);
+            this.button2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(362, 173);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 54);
+            this.button2.Size = new System.Drawing.Size(167, 66);
             this.button2.TabIndex = 2;
             this.button2.Text = "Destroy Server";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(12, 276);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1235, 252);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(586, 257);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AWS Controls";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::FYP_CLOUD_APP.Properties.Resources.AWSLogo;
+            this.pictureBox1.InitialImage = global::FYP_CLOUD_APP.Properties.Resources.AWSLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(183, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::FYP_CLOUD_APP.Properties.Resources.AzureLogo;
+            this.pictureBox2.InitialImage = global::FYP_CLOUD_APP.Properties.Resources.AWSLogo;
+            this.pictureBox2.Location = new System.Drawing.Point(192, -4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 140);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(644, 151);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(550, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Create and destroy an AWS apache web server with the below buttons";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LawnGreen;
+            this.button3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(37, 173);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 66);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Create Server";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Crimson;
+            this.button4.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(381, 173);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 66);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Destroy Server";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(619, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(628, 257);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Azure Controls";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LavenderBlush;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1271, 535);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Cloud Resource Deployment App";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +211,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- FYP_CLOUD_APP/Form1.cs
+++ FYP_CLOUD_APP/Form1.cs
@@ -14,6 +14,7 @@ namespace FYP_CLOUD_APP
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -24,28 +25,148 @@ namespace FYP_CLOUD_APP
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //AWS deploy btn
         {
-            var ps1Apply = @"C:\\FYP_APP\\powershell\\terraformapply.ps1"; //assign the variable the path of the ps1 file
-            var startInfo = new ProcessStartInfo() //used to specify the properties of a process
+            textBox1.AppendText("Please wait...\r\n");
+            var ps1Apply = @"C:\\FYP_APP\\powershell\\TF_AWS_Apply.ps1";
+            var startInfo = new ProcessStartInfo()
             {
-                FileName = "powershell.exe", //run powershell
-                Arguments = $"-NoProfile -ExecutionPolicy unrestricted \"{ps1Apply}\"", //run it with these arguments 
-                UseShellExecute = false //starts directly using no shell
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy unrestricted \"{ps1Apply}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
             };
-            Process.Start(startInfo);
-        }
 
-        private void button2_Click(object sender, EventArgs e)
+            var process = new Process()
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+
+            process.OutputDataReceived += (s, args) =>
+            {
+                
+                if (!string.IsNullOrEmpty(args.Data))
+                {
+                    // Append the output to textBox1
+                    textBox1.Invoke((MethodInvoker)delegate
+                    {
+                        textBox1.AppendText(args.Data + Environment.NewLine);
+                    });
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+        }
+        private void button2_Click(object sender, EventArgs e) //AWS Destroy btn
         {
-            var ps1Destroy = @"C:\\FYP_APP\\powershell\\terraformDestroy.ps1";
+            textBox1.AppendText("Please wait...\r\n");
+            var ps1Destroy = @"C:\\FYP_APP\\powershell\\TF_AWS_Destroy.ps1";
             var startInfo = new ProcessStartInfo()
             {
                 FileName = "powershell.exe",
                 Arguments = $"-NoProfile -ExecutionPolicy unrestricted \"{ps1Destroy}\"",
-                UseShellExecute = false
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
             };
-            Process.Start(startInfo);
+
+            var process = new Process()
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+            process.OutputDataReceived += (s, args) =>
+            {
+                if (!string.IsNullOrEmpty(args.Data))
+                {
+                    // Append the output to textBox1
+                    textBox1.Invoke((MethodInvoker)delegate
+                    {
+                        textBox1.AppendText(args.Data + Environment.NewLine);
+                    });
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Azure deploy btn
+        {
+            textBox1.AppendText("Please wait...\r\n");
+            var ps1Apply = @"C:\\FYP_APP\\powershell\\TF_Azure_Apply.ps1";
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy unrestricted \"{ps1Apply}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            var process = new Process()
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+            process.OutputDataReceived += (s, args) =>
+            {
+                if (!string.IsNullOrEmpty(args.Data))
+                {
+                    // Append the output to textBox1
+                    textBox1.Invoke((MethodInvoker)delegate
+                    {
+                        textBox1.AppendText(args.Data + Environment.NewLine);
+                    });
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Azure Destroy btn
+        {
+            textBox1.AppendText("Please wait...\r\n");
+            var ps1Destroy = @"C:\\FYP_APP\\powershell\\TF_Azure_Destroy.ps1";
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy unrestricted \"{ps1Destroy}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            var process = new Process()
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+            process.OutputDataReceived += (s, args) =>
+            {
+                if (!string.IsNullOrEmpty(args.Data))
+                {
+                    // Append the output to textBox1
+                    textBox1.Invoke((MethodInvoker)delegate
+                    {
+                        textBox1.AppendText(args.Data + Environment.NewLine);
+                    });
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+        
+    }
     }
 }
